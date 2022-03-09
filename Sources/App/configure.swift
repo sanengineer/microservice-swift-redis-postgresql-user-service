a404223a4686e44505cda87f25f09b749a028214
sanengineer <48153104+sanengineer@users.noreply.github.com>
2022-03-09 22:46:04 +0700

    try migrate

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -69,6 +69,24 @@ public func configure(_ app: Application) throws {
 
         // app.databases.use(.postgres()
     }
+
+     let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+
+    // Only add this if you want to enable the default per-route logging
+    let routeLogging = RouteLoggingMiddleware(logLevel: .info)
+
+    // Add the default error middleware
+    let error = ErrorMiddleware.default(environment: app.environment)
+    // Clear any existing middleware.
+    app.middleware = .init()
+    app.middleware.use(cors)
+    app.middleware.use(routeLogging)
+    app.middleware.use(error)
     
     app.logger.logLevel = .debug
     app.http.server.configuration.port = port
